8d30507380b30335287e3a55fe2d07846551fb7f
hoseokryu <dkflfkdeh@naver.com>
2018-03-22 13:18:32 +0900

    fexecutable

---
+++ cmake-build-debug/CMakeFiles/practice_tutorial_pendulum.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/practice_tutorial_pendulum.dir/main.cpp.o"
+  "practice_tutorial_pendulum.pdb"
+  "practice_tutorial_pendulum"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/practice_tutorial_pendulum.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
